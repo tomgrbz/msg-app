ade5efc284f41e428ba78ed45b5449a322998b0a
tomgrbz <tomgrbz@gmail.com>
2022-10-31 22:38:24 -0400

    GUI changes, room button now is daisy-ui

--- client/src/components/roommodal.tsx
+++ client/src/components/roommodal.tsx
@@ -16,20 +16,23 @@ export const RoomModal = ({socket, user, rooms}: { socket: Socket, user: string,
             {rooms.filter((v, i, a) => a.indexOf(v) === i).map((v, i) => {
                 return (<Link
                     to={`/chat/${v}/${user}`}
-                              key={i}>
+                    key={i}>
                     <p>Change to room: {v}</p>
                 </Link>)
             })}
             <p>Please Enter the new room you'd like to join!</p>
-            <input type="text" autoComplete="off" placeholder="Room" onChange={e => {
-                setNewRoom(e.target.value)
+            <div className="pt-3">
+                <input className="input input-bordered input-accent" type="text" autoComplete="off" placeholder="Room"
+                       onChange={e => {
+                           setNewRoom(e.target.value)
 
-            }}/>
-            <Link to={`/chat/${newRoom}/${user}`} key={newRoom}>
-                <Button onClick={joinRoom}>
-                    Join
-                </Button>
-            </Link>
+                       }}/>
+                <Link to={`/chat/${newRoom}/${user}`} key={newRoom}>
+                    <button className="btn ml-2 px-5" onClick={joinRoom}>
+                        Join
+                    </button>
+                </Link>
+            </div>
 
         </div>
     )
